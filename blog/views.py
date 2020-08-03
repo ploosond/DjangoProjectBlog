cc9a76ed80553b6ca692ba909beba2a67645b404
Devkota Prajwol <prajwol.devkota016@gmail.com>
2020-08-03 02:26:12 +0200

    Templates added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Devkota Prajwol',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 3, 2020'
+    },
+    {
+        'author': 'Devkota Ashish',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 4, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
