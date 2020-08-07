cf39684b7029454d050d451f7099eac14f6a4196
Devkota Prajwol <prajwol.devkota016@gmail.com>
2020-08-07 03:09:26 +0200

    Database and Migrations

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Devkota Prajwol',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 3, 2020'
-    },
-    {
-        'author': 'Devkota Ashish',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 4, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
